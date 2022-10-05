6292450a54ac37a11cebf1de55a63cb767175a17
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-10-04 21:43:36 -0300

    Junit estudo para prova parte3

--- Calculator/src/test/java/CalculatorTest.java
+++ Calculator/src/test/java/CalculatorTest.java
@@ -1,6 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+
+https://www.vogella.com/tutorials/JUnit/article.html
  */
 
 import com.mycompany.calculator.Calculator;
