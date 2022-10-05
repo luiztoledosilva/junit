3d4941c4d903526dd3b6036c87f848ff6b49b687
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-10-04 21:41:12 -0300

    Junit estudo para prova parte2

---
+++ Calculator/src/test/java/CalculatorTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.mycompany.calculator.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+/**
+ *
+ * @author Usuario
+ */
+class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach                                         
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test                                               
+    @DisplayName("Simple multiplication should work")   
+    void testMultiply() {
+        assertEquals(20, calculator.multiply(4, 5),     
+                "Regular multiplication should work");  
+    }
+
+    @RepeatedTest(5)                                    
+    @DisplayName("Ensure correct handling of zero")
+    void testMultiplyWithZero() {
+        assertEquals(0, calculator.multiply(0, 5), "Multiple with zero should be zero");
+        assertEquals(0, calculator.multiply(5, 0), "Multiple with zero should be zero");
+    }
+}
